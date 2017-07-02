6c427d2c75a6740969d87407255bb46d3fb172bb
Henry Nguyen <haiho1992@gmail.com>
2017-07-02 23:17:51 +0700

    Add inputRef prop Exposes the react native's TextInput.

--- components/TextInput.js
+++ components/TextInput.js
@@ -5,35 +5,35 @@ import { connectStyle } from '@shoutem/theme';
 import { connectAnimation } from '@shoutem/animation';
 
 class TextInput extends Component {
-  render() {
-    const { props } = this;
-    const style = {
-      ...props.style,
-    };
-    delete style.placeholderTextColor;
-    delete style.selectionColor;
-    delete style.underlineColorAndroid;
+	render() {
+		const { inputRef, ...props } = this.props
+		const style = {
+			...props.style
+		};
+		delete style.placeholderTextColor;
+		delete style.selectionColor;
+		delete style.underlineColorAndroid;
 
-    return (
-      <RNTextInput
-        {...props}
-        style={style}
-        placeholderTextColor={props.style.placeholderTextColor}
-        selectionColor={props.style.selectionColor}
-        underlineColorAndroid={props.style.underlineColorAndroid}
-      />
-    );
-  }
+		return (
+			<RNTextInput
+				{...props}
+				style={style}
+        ref={inputRef}
+				placeholderTextColor={props.style.placeholderTextColor}
+				selectionColor={props.style.selectionColor}
+				underlineColorAndroid={props.style.underlineColorAndroid}
+			/>
+		);
+	}
 }
 
 TextInput.propTypes = {
-  ...RNTextInput.propTypes,
-  style: React.PropTypes.object,
+	...RNTextInput.propTypes,
+	style: React.PropTypes.object,
+  inputRef: React.PropTypes.func
 };
 
 const AnimatedTextInput = connectAnimation(TextInput);
 const StyledTextInput = connectStyle('shoutem.ui.TextInput')(AnimatedTextInput);
 
-export {
-  StyledTextInput as TextInput,
-};
+export { StyledTextInput as TextInput };
